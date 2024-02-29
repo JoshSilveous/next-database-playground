a88bdd83d93f1e65f5be25fb951a70ceb5a900c3
Joshua Silveous <JoshSilveous@gmail.com>
2024-02-29 12:57:11 -0500

    messed around

--- lib/components/DynTable.tsx
+++ lib/components/DynTable.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler, useRef } from 'react'
 import styles from './DynTable.module.scss'
 
 export function DynTable({ data }: { data: object[] }) {
@@ -8,25 +9,44 @@ export function DynTable({ data }: { data: object[] }) {
 			return <div className={styles.item}>{item[header]}</div>
 		})
 
+		const resizerMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+			if (e.button === 0) {
+				const columnNode = (e.target as HTMLDivElement).previousSibling as HTMLDivElement
+				const startingWidth = columnNode.clientWidth + 'px'
+				columnNode.style.width = startingWidth
+				;(columnNode.parentNode as HTMLDivElement).style.userSelect = 'none'
+
+				const mouseMoveHandler: (this: Window, ev: MouseEvent) => any = (e) => {
+					if (e.clientX !== columnNode.offsetLeft + columnNode.clientWidth) {
+						const difference =
+							e.clientX - (columnNode.offsetLeft + columnNode.clientWidth)
+						columnNode.style.width =
+							parseInt(columnNode.style.width) + difference + 'px'
+					}
+				}
+
+				window.addEventListener('mousemove', mouseMoveHandler)
+				window.addEventListener('mouseup', () => {
+					;(columnNode.parentNode as HTMLDivElement).style.userSelect = ''
+					window.removeEventListener('mousemove', mouseMoveHandler)
+				})
+			}
+		}
+
 		return (
 			<>
 				<div className={styles.column}>
 					<div className={styles.header}>{header}</div>
 					{columnData}
 				</div>
-				<div className={styles.resizer} />
+				<div
+					className={styles.resizer}
+					onMouseDown={resizerMouseDown}
+					// onMouseUp={resizerMouseUp}
+				/>
 			</>
 		)
 	})
 
-	const jsxData = data.map((row, index) => {
-		return (
-			<tr key={index}>
-				{Object.values(row).map((item, index) => {
-					return <td key={index}>{item}</td>
-				})}
-			</tr>
-		)
-	})
 	return <div className={styles.container}>{columns}</div>
 }
